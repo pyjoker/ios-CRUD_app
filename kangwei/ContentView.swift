d73692ac8100f5588c594ea380ea14a3a24b1790
kangwei <kangwei>
2019-11-30 14:29:18 +0800

    Initial Commit

---
+++ kangwei/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  kangwei
+//
+//  Created by Customer on 2019/11/30.
+//  Copyright © 2019 kangwei. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
